54a40d292ef954ee54a3280574652b4169cb5b96
Edycarlos018 <edycarlos@gmail.com>
2019-01-07 14:44:40 -0500

    More work on PostProduct w/ Stephen

--- Backend/SIS.API/Controllers/Product/ProductController.cs
+++ Backend/SIS.API/Controllers/Product/ProductController.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using RedStarter.API.DataContract.Product;
+using RedStarter.Business.DataContract.Product;
 
 namespace RedStarter.API.Controllers.Product
 {
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private readonly IMapper _mapper;
+
+        public ProductController(IMapper mapper)
         {
-            return View();
+            _mapper = mapper;
         }
+
+        [HttpPost]
+        public async Task<IActionResult> PostProduct(ProductCreateRequest request)
+        {
+            var dto = _mapper.Map<ProductCreateDTO>(request);
+            dto.DateCreated = DateTime.Now;
+
+            return Ok();
+        }
+        
     }
 }
